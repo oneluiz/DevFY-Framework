db30c700b3587d64eb9644ff6b128657f4b083a8
One <luizcortesj@gmail.com>
2017-02-12 09:41:29 -0600

    Update framework

--- Views/home/index.php
+++ Views/home/index.php
@@ -1,1 +1,15 @@
-<h1>Hello, world!</h1>
+<?php
+function Carga(){ 
+	list($usec, $sec) = explode(" ", microtime()); 
+	return ((float)$usec + (float)$sec); 
+}
+$tiempo_inicio = Carga(); 
+$tiempo_final = Carga();
+$tiempo = number_format($tiempo_final - $tiempo_inicio, 5); 
+echo "<center><pre>Esta p&aacute;gina fue generada en {$tiempo} milisegundos</pre></center>"; 
+?>
+<center>
+<a href="<?php echo URL; ?>"><img src="https://avatars0.githubusercontent.com/u/3411968" width="25%"></img></a>
+<h3>..Y este es mi Framework</h3>
+<a href="https://github.com/oneluiz/php/"><h3><i class="fa fa-github"></i> Github</h3></a>
+</center>
